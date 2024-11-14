484a952bbbbb127450317812620bcc7818677aaf
Giovanni-Morales-Camacho <103343387+Giovanni-Morales-Camacho@users.noreply.github.com>
2024-11-14 17:29:34 -0600

    Modificación en el hola.html

--- src/main/java/com/example/holadocker/controller/HomeController.java
+++ src/main/java/com/example/holadocker/controller/HomeController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
     
-    @GetMapping("/hola")
+    @GetMapping("/hola") //esta es la url a abirse dentro del puerto 8080
     public String holaMundo() {
-        return "hola";
+        return "hola"; //este es el html que tomará para mostrar
     }
     
     @GetMapping("/")
